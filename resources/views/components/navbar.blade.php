cd0903761372ae6676a0c6b845d5ed0de3c9b868
Nicolina <nicolina.vesco@gmail.com>
2026-02-04 13:52:10 +0100

    Start Creazione navbar, layout, view

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+
+        <img src="https://placehold.co/120x40?text=Logo" alt="" class="logo">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto gap-4 align-items-center">
+                <li class="nav-item">
+                    <a class="nav-link active fw-bold" href="#">Accedi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Registrati</a>
+                </li>
+                <li class="nav-item">
+                    <button class=" btn-navbar fw-bold">
+                        <i class="fa-solid fa-plus" style="color: #ff0000;"></i> Inserisci annuncio
+                    </button>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
