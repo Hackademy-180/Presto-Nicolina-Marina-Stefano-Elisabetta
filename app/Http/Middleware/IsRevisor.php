6882cbd9b62aa685afe7eb7c35bfc854c2e6a355
Stefano De Iuliis <stefano.deiuliis.98@gmail.com>
2026-02-09 23:31:06 +0100

    check funzioni mailable con autorizazione revisor

--- app/Http/Middleware/IsRevisor.php
+++ app/Http/Middleware/IsRevisor.php
@@ -13,7 +13,7 @@ class IsRevisor
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
+      */
     public function handle(Request $request, Closure $next): Response
     {
         if(Auth::check()&& Auth::user()->is_revisor){
