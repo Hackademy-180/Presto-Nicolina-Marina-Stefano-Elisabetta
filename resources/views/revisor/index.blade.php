be3c731211b325885f958fb6bc7dcd39197e284b
Nicolina <nicolina.vesco@gmail.com>
2026-02-10 10:43:43 +0100

    Agiustata la index dettagli articolo e btn

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -12,64 +12,64 @@
 
         <div class="row">
             <div class="rounded shadow bg-body-secondary">
-                <h1 class="display-5 text-center pb-2">
+                <h1 class="text-center pb-3">
                     Revisor dashboard
                 </h1>
             </div>
         </div>
-    </div>
-    @if ($article_to_check)
-    <div class="row justify-content-center pt-5">
-        <div class="col-md-8">
-            <div class="row justify-content-center">
-                @for ($i = 0; $i < 6; $i++)
-                <div class="col-6 col-md-4 mb-4 text-center">
-                    <img src="https://picsum.photos/300" alt="immagine segniaposto" class="img-fluid rounded shadow">
+        @if ($article_to_check)
+        <div class="row justify-content-center pt-5">
+            <div class="col-md-8">
+                <div class="row justify-content-center">
+                    @for ($i = 0; $i < 6; $i++)
+                    <div class="col-6 col-md-4 mb-4 text-center">
+                        <img src="https://picsum.photos/300" alt="immagine segniaposto" class="img-fluid rounded shadow">
+                    </div>
+                    @endfor
                 </div>
-                @endfor
             </div>
-        </div>
-        <div class="col-md-4 d-flex flex-column justify-content-between">
-            <div>
-                <h3>{{ $article_to_check->title }}</h3>
-                <h4>{{ $article_to_check->user->name }}</h4>
-                <h4>{{ $article_to_check->price }}€</h4>
-                <h4 class="fst-italic text-muted">
-                    {{ $article_to_check->category->name }}
-                </h4>
-                <p class="fs-6">{{ $article_to_check->description }}</p>
-            </div>
-            
-            <div class="d-flex pb-4 justify-content-around">
-                <form action="{{route("reject", ["article"=>$article_to_check])}}" method="POST">
-                    @csrf
-                    @method("PATCH")
-                    <button class="btn btn-danger py-2 px-5 fw-bold">
-                        Rifiuta
-                    </button>
-                </form>
+            <div class="col-md-4 d-flex flex-column justify-content-evenly align-items-center">
+                <div class="w-75 text-center mt-5">
+                    <h3>{{ $article_to_check->title }}</h3>
+                    <h4>{{ $article_to_check->user->name }}</h4>
+                    <h4>{{ $article_to_check->price }}€</h4>
+                    <h4 class="fst-italic text-muted">
+                        {{ $article_to_check->category->name }}
+                    </h4>
+                    <p class="fs-6 mt-5">{{ $article_to_check->description }}</p>
+                </div>
                 
-                <form action="{{route("accept", ["article"=>$article_to_check])}}" method="POST">
-                    @csrf
-                    @method("PATCH")
-                    <button class="btn btn-success py-2 px-5 fw-bold">
-                        Accetta
-                    </button>
-                </form>
+                <div class="d-flex pb-4 gap-3 justify-content-center">
+                    <form action="{{route("reject", ["article"=>$article_to_check])}}" method="POST">
+                        @csrf
+                        @method("PATCH")
+                        <button class="btn btn-danger py-2 px-5 fw-bold">
+                            Rifiuta
+                        </button>
+                    </form>
+                    
+                    <form action="{{route("accept", ["article"=>$article_to_check])}}" method="POST">
+                        @csrf
+                        @method("PATCH")
+                        <button class="btn btn-success py-2 px-5 fw-bold">
+                            Accetta
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-    @else
-    <div class="row justify-content-center align-items-center height-custom text-center">
-        <div class="col-12">
-            <h1 class="fst-italic display-4">
-                Nessun articolo da revisionare
-            </h1>
-            <a href="{{ route('home') }}" class="mt-5 btn btn-success">
-                Torna all'homepage
-            </a>
+        @else
+        <div class="row justify-content-center align-items-center height-custom text-center">
+            <div class="col-12">
+                <h1 class="fst-italic display-4">
+                    Nessun articolo da revisionare
+                </h1>
+                <a href="{{ route('home') }}" class="mt-5 btn btn-danger">
+                    Torna all'homepage
+                </a>
+            </div>
         </div>
+        @endif
     </div>
-    @endif
 </div>
 </x-layout>
